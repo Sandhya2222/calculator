d519204af9d277462f76c49ec6c51b107d573541
Sandhya2222 <sandhya4sobha@gmail.com>
2021-06-14 23:30:55 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+let screen=document.getElementById('screen');
+let buttons=document.querySelectorAll('button')
+let screenValue = '';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;
+        screen.value=buttonText;
+        console.log('button',buttonText)
+        if(buttonText=='X'){
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+            console.log('ghgj',screen.value)
+        }
+
+        else if(buttonText=='+'){
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+        else if(buttonText=='-'){
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText=='/'){
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+        else if(buttonText=='='){
+            screen.value = eval(screenValue);
+
+
+        }
+        else if(buttonText=='C'){
+            screenValue ="";
+            screen.value = screenValue;
+            
+
+        }
+        else{
+            screenValue +=buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
+    
